24d74df49bba55f85881c4a9c1d2e889279770fe
Peter Stein <peter.sd@bolsista.pti.org.br>
2016-10-25 00:56:18 -0300

    declaring and printing variables

---
+++ introduction/declareVariable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+	int A = 7;
+	int B = -9;
+	float C = 1.3784;
+	long D = 65438;
+
+	printf("Value of A: %d\nValue of B: %d\n", A, B);
+	printf("Value of C: %f\nValue of D: %ld\n", C, D);
+	return 0;
+}
